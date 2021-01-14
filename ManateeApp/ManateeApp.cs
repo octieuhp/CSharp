89235fc7dafd8d1498e55529a0cd524ba275ae7c
unknown <octieugm@gmail.com>
2021-01-14 15:10:15 +0700

    add problem and source code for Matanee problem

---
+++ ManateeApp/ManateeApp.cs
@@ -0,0 +1,72 @@
+﻿/* ManateeApp.cs
+ * This is the client program that uses the
+ *  ManateeSighting class. Users are prompted
+ *  for location, date, and sightings. The
+ *  ManateeSighting class is tested using
+ *  this class by calling many of the
+ *  methods and properties.
+ */
+using System;
+using static System.Console;
+namespace ManateeApp
+{
+    public class ManateeApp
+    {
+        static void Main(string[] args)
+        {
+            string location;
+            int sightingcnt;
+            string[] dArray = new string[20];
+            int[] manateeCnt = new int[20];
+            char enterMoreData = 'Y';
+            ManateeSighting m;
+
+            do
+            {
+                sightingcnt = GetData(out location, dArray, manateeCnt);
+                m = new ManateeSighting(location, dArray, manateeCnt, sightingcnt);
+                Clear();
+                WriteLine(m);
+                Write("\n\n\n\nDo you want to enter more " +
+                        "data - (Enter y or n)? ");
+                if (char.TryParse(ReadLine(), out enterMoreData) == false)
+                    WriteLine("Invalid data entered - No " +
+                                "recorded for your response ");
+            }
+            while (enterMoreData == 'Y' || enterMoreData == 'y');
+        }
+
+        public static int GetData(out string location, string[] dArray, int[] manateeCnt)
+        {
+            int i, loopCnt;
+            Clear();
+            Write("Location: ");
+            location = ReadLine();
+            Write("How many records for {0}? ", location);
+            string inValue = ReadLine();
+            if (int.TryParse(inValue, out loopCnt) == false)
+                WriteLine("Invalid data entered - " +
+                        "0 recorded for number of records");
+            for (i = 0; i < loopCnt; i++)
+            {
+                Write("\nDate (mm/dd/yyyy): ");
+                dArray[i] = ReadLine();
+                if (dArray[i] == "")
+                {
+                    WriteLine("No date entered - " +
+                                "Unknown recorded " +
+                                "for sightings");
+                    dArray[i] = "Unknown";
+                }
+                Write("Number of Sightings: ");
+                inValue = ReadLine();
+                if (int.TryParse(inValue, out manateeCnt[i]) == false)
+                    WriteLine("Invalid data entered - 0 " +
+                                "recorded for manatee sightings ");
+            }
+
+            return i;
+        }
+    }
+
+}
---
+++ ManateeApp/ManateeSighting.cs
@@ -0,0 +1,187 @@
+﻿/* ManateeSighting.cs
+ * This class defines manatee characteristics to include
+ * location, count, and date of sightings. Methods to
+ * determine the month with most sightings and
+ * average number of sightings per location included.
+ */
+using System;
+
+namespace ManateeApp
+{
+    public class ManateeSighting
+    {
+        private string location;
+        private string[] sightDate;
+        private int[] manateeCount;
+
+        // Constructors
+        public ManateeSighting()
+        {
+
+        }
+
+        public ManateeSighting(string loc)
+        {
+            location = loc;
+        }
+
+        public ManateeSighting(string loc, string [] date, int [] count)
+        {
+            sightDate = new string[date.Length];
+            manateeCount = new int[count.Length];
+            Array.Copy(date, 0, sightDate, 0, date.Length);
+            Array.Copy(count, 0, manateeCount, 0, manateeCount.Length);
+
+            location = loc;
+        }
+
+        public ManateeSighting(string loc, string[] date, int [] cnt, int numOfFlights)
+        {
+            sightDate = new string[numOfFlights];
+            manateeCount = new int[numOfFlights];
+            Array.Copy(date, 0, sightDate, 0, numOfFlights);
+            Array.Copy(cnt, 0, manateeCount, 0, numOfFlights);
+            location = loc;
+        }
+
+        // Properties
+        public string Location
+        {
+            get
+            {
+                return location;
+            }
+            set
+            {
+                location = value;
+            }
+        }
+
+        public string[] SightDate
+        {
+            get
+            {
+                return sightDate;
+            }
+            set
+            {
+                sightDate = value;
+            }
+        }
+
+        public int[] ManateeCount
+        {
+            get
+            {
+                return manateeCount;
+            }
+            set
+            {
+                manateeCount = value;
+            }
+        }
+
+        // Determine what the average number of 
+        // sightings is per location
+        public double CalculateAvg()
+        {
+            double avg;
+            int cntOfValidEntries;
+            int total = 0;
+            foreach (int c in manateeCount)
+                total += c;
+            cntOfValidEntries = TestForZeros();
+            avg = (double)total / cntOfValidEntries;
+            return avg;
+        }
+
+        // To avoid skewing average, return number
+        // of cells with nonzero values
+        public int TestForZeros()
+        {
+            int numberOfTrueSightings = 0;
+            foreach (int cnt in manateeCount)
+                if (cnt != 0)
+                    numberOfTrueSightings++;
+            return numberOfTrueSightings;
+        }
+
+        // Return an index where the largest
+        // number of sightings is stored
+        public int GetIndexOfMostSightings()
+        {
+            int maxCntIndex = 0;
+            for (int i = 1; i < manateeCount.Length; i++)
+                if (manateeCount[i] > manateeCount[maxCntIndex])
+                    maxCntIndex = i;
+            return maxCntIndex;
+        }
+
+        // Returns a count of the most sighted
+        public int GetMostSightings()
+        {
+            return manateeCount[GetIndexOfMostSightings()];
+        }
+
+        // Returns the date when most sightings occurred
+        public string GetDateWithMostSightings()
+        {
+            return sightDate[GetIndexOfMostSightings()];
+        }
+
+        // Returns the name of the month when the
+        // highest sightings occured
+        public string getMonthWithMostSightings()
+        {
+            return ReturnMonth(sightDate[GetIndexOfMostSightings()]);
+        }
+
+        // Computes the average for a given month
+        public double ComputerAverageForMonth(string mon)
+        {
+            int total = 0;
+            int days = 0;
+            double monAverage;
+            for(int i = 0; i < sightDate.Length; i++)
+            {
+                if (sightDate[i].StartsWith(mon))
+                {
+                    total += manateeCount[i];
+                    days++;
+                }
+            }
+            if (days > 0)
+                monAverage = (double)total / days;
+            else
+                monAverage = 0;
+            return monAverage;
+        }
+
+        // Given a date in the format of mm/dd/yyyy
+        // the name of the month is returned
+        public string ReturnMonth(string someDate)
+        {
+            string[] monthName =
+                {"January", "February", "March",
+                "April", "May", "June", "July", "August",
+                "September", "October", "November", "December"};
+            string[] dateParts = someDate.Split('/');
+            dateParts[0] = dateParts[0].TrimStart('0');
+            return monthName[Convert.ToInt32(dateParts[0]) - 1];
+        }
+
+        public override string ToString()
+        {
+            return "\tLocation: " + location +
+                    "\n\nAverage Number " +
+                    "of Sightings: \t" +
+                    CalculateAvg().ToString("F1") +
+                    "\n\nMonth name for the" +
+                    "\nDate of Most Sightings: \t\t" +
+                    getMonthWithMostSightings() +
+                    "\nCount for " +
+                    GetDateWithMostSightings() + ":\t\t" +
+                    GetMostSightings() ; 
+        }
+    }
+}

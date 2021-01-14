7e1630c7a3f238598b3e707140826be993b12279
unknown <octieugm@gmail.com>
2021-01-14 15:25:04 +0700

    add solutio and source code for Loan Problmem

---
+++ LoanApp/Loan.cs
@@ -0,0 +1,167 @@
+﻿
+/* Loan.cs
+ * Creates fileds for the amount of loan, interest
+ * rate, and number of years. Calculates payment amount
+ * and produces an amortizatio schedule
+ */
+using System;
+
+namespace LoanApp
+{
+    public class Loan
+    {
+        private double loanAmount;
+        private double rate;
+        private int numPayments;
+        private double balance;
+        private double totalInterestPaid;
+        private double paymentAmout;
+        private double principal;
+        private double monthInterest;
+
+        // Default constructor
+        public Loan()
+        { }
+
+        // Constructor
+        public Loan(double loan, double interestRate, int years)
+        {
+            loanAmount = loan;
+            if (interestRate < 1)
+                rate = interestRate;
+            else
+                rate = interestRate / 100;
+            numPayments = 12 * years;
+            totalInterestPaid = 0;
+            DeterminePaymentAmount();
+        }
+
+        //Property accessing payment amount
+        public double PaymentAmout
+        {
+            get
+            {
+                return paymentAmout;
+            }
+        }
+
+        // Property setting and returning loan amount
+        public double LoanAmount
+        {
+            set
+            {
+                loanAmount = value;
+            }
+            get
+            {
+                return loanAmount;
+            }
+        }
+
+        // Property setting and returning rat
+        public double Rate
+        {
+            set
+            {
+                rate = value;
+            }
+            get
+            {
+                return rate;
+            }
+        }
+
+        // Property to set the numPayments, given years
+        // to finance. Returns the number of years using
+        // number of payments
+        public int Years
+        {
+            set
+            {
+                numPayments = value;
+            }
+            get
+            {
+                return numPayments / 12;
+            }
+        }
+
+        //Property for accessing total interest
+        public double TotalInterestPaid
+        {
+            get
+            {
+                return totalInterestPaid;
+            }
+        }
+
+        // Determine payment amount based on number of
+        // years, loan amount, and rate
+        public void DeterminePaymentAmount()
+        {
+            double term;
+            term = Math.Pow((1 + rate / 12.0), numPayments);
+            paymentAmout = (loanAmount * rate / 12.0 * term) / (term - 1.0);
+        }
+
+        // Returns string containing amortizaition table
+        public string ReturnAmortizationSchedule()
+        {
+            string aSchedule = "Month\t\tInt.\t\tPrin.\t\tNew";
+            aSchedule += "\nNo.\t\tPd.\t\tPd.\t\tBalance\n";
+            aSchedule += "-----\t\t-----\t\t-----\t" + "\t--------\n";
+
+            balance = loanAmount;
+            for (int month = 1; month <= numPayments;month++)
+            {
+                CalculateMonthCharges(month, numPayments);
+                aSchedule += month + "\t" +
+                            monthInterest.ToString("N2") + "\t" +
+                            principal.ToString("N2") + "\t" +
+                            balance.ToString("C") + "\n";
+            }
+            return aSchedule;
+        }
+
+        // Calculates monthly interest and new balance.
+        public void CalculateMonthCharges(int month, int numPayments)
+        {
+            double payment = paymentAmout;
+            monthInterest = rate / 12 * balance;
+            if (month == numPayments)
+            {
+                principal = balance;
+                payment = balance + monthInterest;
+            }
+            else
+            {
+                principal = payment - monthInterest;
+            }
+            balance -= principal;
+        }
+
+        // Calcultes interest paid over life of loan
+        public void DetermineTotalInterestPaid()
+        {
+            totalInterestPaid = 0;
+            balance = loanAmount;
+            for (int month = 1; month <= numPayments; month++)
+            {
+                CalculateMonthCharges(month, numPayments);
+                totalInterestPaid += monthInterest;
+            }
+        }
+
+        // Return information about the loan
+        public override string ToString()
+        {
+            return "\nLoan Amount: " +
+                    loanAmount.ToString("C") +
+                    "\nInterest Rate: " + rate +
+                    "\nNumber of Years for Loan: " +
+                    (numPayments / 12) +
+                    "\nMonthly payment: " +
+                    paymentAmout.ToString("C");
+        }
+    }
+}
---
+++ LoanApp/LoanApp.cs
@@ -0,0 +1,107 @@
+﻿/*LoanApp.cs
+ * Used for testing Loan class.
+ * Prompts user for loan amount,
+ * interest rate, and time period
+ * for loan. Calls method to display
+ * payment amount and amortization
+ * schedule. Allows more than 
+ * on loan Calculation
+ */
+using System;
+using static System.Console;
+namespace LoanApp
+{
+    class LoanApp
+    {
+        static void Main(string[] args)
+        {
+            int years;
+            double loanAmount;
+            double interestRate;
+            string inValue;
+            char anotherLoan = 'N';
+
+            do
+            {
+                GetInputValues(out loanAmount, out interestRate, out years);
+                Loan ln = new Loan(loanAmount, interestRate, years);
+                WriteLine();
+                Clear();
+                WriteLine(ln);
+                WriteLine();
+                WriteLine(ln.ReturnAmortizationSchedule());
+                ln.DetermineTotalInterestPaid();
+                WriteLine("Payment Amount: {0:C}", ln.PaymentAmout);
+                WriteLine("Interest Paid over Life " +
+                            "of Loan: {0:C}",
+                            ln.TotalInterestPaid);
+                Write("Do another Calculation? (Y or N)");
+                inValue = ReadLine();
+                anotherLoan = Convert.ToChar(inValue);
+            }
+            while ((anotherLoan == 'Y') || (anotherLoan == 'y'));
+        }
+
+        // Prompts user for loan data
+        static void GetInputValues(out double loanAmount, out double interestRate, out int years)
+        {
+            Clear();
+            loanAmount = GetLoanAmount();
+            interestRate = GetInterestRate();
+            years = GetYears();
+        }
+
+        static double GetLoanAmount()
+        {
+            string sValue;
+            double loanAmount;
+            Write("Please enter the loan amount: ");
+            sValue = ReadLine();
+            while ((double.TryParse(sValue, out loanAmount) == false) 
+                            || loanAmount < 1 || loanAmount > 500000)
+            {
+                WriteLine("Invalid data entered " + "for loan amount");
+                Write("\nPlease re-enter the loan amount " + " (less than 500,000): ");
+                sValue = ReadLine();
+            }
+
+            return loanAmount;
+        }
+
+        static double GetInterestRate()
+        {
+            string sValue;
+            double interestRate;
+            Write("Please enter interest rate (as a " +
+                    "decimal value - i.e. . 06): ");
+            sValue = ReadLine();
+            while((double.TryParse(sValue, out interestRate) == false)
+                        || interestRate < 0 || interestRate > 1)
+            {
+                Write("\nInvalid data entered for intereset rate " +
+                        " (decimal value - i.e. 0.06):");
+                Write("\nPlease re-enter the interest rate: ");
+                sValue = ReadLine();
+            }
+
+            return interestRate;
+        }
+
+        static int GetYears()
+        {
+            string sValue;
+            int years;
+            Write("Please enter the number of the years " +
+                    "for the loan: ");
+            sValue = ReadLine();
+            while((int.TryParse(sValue, out years) == false)
+                        || years < 1 || years > 30)
+            {
+                Write("\nInvalid data entered for years");
+                Write("\nPlease re-enter the years (1-30): ");
+                sValue = ReadLine();
+            }
+            return years;
+        }
+    }
+}
